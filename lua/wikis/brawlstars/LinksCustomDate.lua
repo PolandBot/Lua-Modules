e9fda630783f3472690189abac1b0c8755092b46
PolandBot <133791350+PolandBot@users.noreply.github.com>
2025-05-11 20:11:44 +0500

    Create LinksCustomDate.lua

---
+++ lua/wikis/brawlstars/LinksCustomDate.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- wiki=counterstrike
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		matcherino = {'https://matcherino.com/supercell/tournaments/'}
+		}
+	},
+}
