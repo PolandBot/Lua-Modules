4dc71e93b536c12829051c4e48f3fe0e4e05e9e3
PolandBot <133791350+PolandBot@users.noreply.github.com>
2025-05-11 19:58:27 +0500

    MatchExternalLinks.lua

--- lua/wikis/brawlstars/MatchExternalLinks.lua
+++ lua/wikis/brawlstars/MatchExternalLinks.lua
@@ -17,28 +17,6 @@ return {
 	},
 	{},
 	{
-		name = 'esl',
-		icon = 'ESL 2019 icon lightmode.png',
-		iconDark = 'ESL 2019 icon darkmode.png',
-		prefixLink = 'https://play.eslgaming.com/match/',
-		label = 'Matchpage and Stats on ESL Play',
-		isMapStats = true
-	},
-	{
-		name = 'faceit',
-		icon = 'FACEIT-icon.png',
-		prefixLink = 'https://www.faceit.com/en/match/room/',
-		label = 'Match Room and Stats on FACEIT',
-		isMapStats = true
-	},
-	{
-		name = 'lpl',
-		icon = 'letsplay.live 2024 icon lightmode.png',
-		iconDark = 'letsplay.live 2024 icon darkmode.png',
-		prefixLink = 'https://gg.letsplay.live/report-score/',
-		label = 'Matchpage on letsplay.live',
-	},
-	{
 		name = 'challengermode',
 		icon = 'Challengermode icon.png',
 		prefixLink = 'https://www.challengermode.com/games/',
@@ -54,4 +32,3 @@ return {
 		label = 'Matchpage on Matcherino'
 	}
 }
-
