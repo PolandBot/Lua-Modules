69c4c21d1133263f7f81a7be6044088d53861082
PolandBot <133791350+PolandBot@users.noreply.github.com>
2025-05-11 21:19:11 +0500

    MatchSummary.lua

--- lua/wikis/brawlstars/MatchSummary.lua
+++ lua/wikis/brawlstars/MatchSummary.lua
@@ -29,6 +29,32 @@ function CustomMatchSummary.getByMatchId(args)
 end
 
 ---@param match MatchGroupUtilMatch
+---@param footer MatchSummaryFooter
+---@return MatchSummaryFooter
+function CustomMatchSummary.addToFooter(match, footer)
+	local vods = {}
+	local secondVods = {}
+	if Logic.isNotEmpty(match.links.vod2) then
+		for _, vod2 in ipairs(match.links.vod2) do
+			local link, gameIndex = unpack(vod2)
+			secondVods[gameIndex] = Array.map(mw.text.split(link, ','), String.trim)
+		end
+		match.links.vod2 = nil
+	end
+	for index, game in ipairs(match.games) do
+		if game.vod then
+			vods[index] = game.vod
+		end
+	end
+
+	if not Table.isEmpty(vods) or not Table.isEmpty(match.links) or not Logic.isEmpty(match.vod) then
+		return CustomMatchSummary._createFooter(match, vods, secondVods)
+	end
+
+	return footer
+end
+
+---@param match MatchGroupUtilMatch
 ---@return MatchSummaryBody
 function CustomMatchSummary.createBody(match)
 	local showCountdown = match.timestamp ~= DateExt.defaultTimestamp
@@ -44,6 +70,120 @@ function CustomMatchSummary.createBody(match)
 		MatchSummaryWidgets.Mvp(match.extradata.mvp),
 		MatchSummaryWidgets.CharacterBanTable{bans = characterBansData, date = match.date}
 	)}
+end
+
+function CustomMatchSummary._createFooter(match, vods, secondVods)
+	local footer = MatchSummary.Footer()
+
+	local separator = '<b>·</b>'
+
+	local function addFooterLink(icon, iconDark, url, label, index)
+		if icon == 'stats' then
+			icon = index ~= 0 and 'Match Info Stats' .. index .. '.png' or 'Match Info Stats.png'
+		end
+		if index > 0 then
+			label = label .. ' for Game ' .. index
+		end
+
+		icon = 'File:' .. icon
+		if iconDark then
+			iconDark = 'File:' .. iconDark
+		end
+
+		footer:addLink(url, icon, iconDark, label)
+	end
+
+	local function addVodLink(gamenum, vod, part)
+		if vod then
+			gamenum = (gamenum and match.bestof > 1) and gamenum or nil
+			local htext
+			if part then
+				if gamenum then
+					htext = 'Watch Game ' .. gamenum .. ' (part ' .. part .. ')'
+				else
+					htext = 'Watch VOD (part ' .. part .. ')'
+				end
+			end
+			footer:addElement(VodLink.display{
+				gamenum = gamenum,
+				vod = vod,
+				htext = htext
+			})
+		end
+	end
+
+	-- Match vod
+	if Table.isNotEmpty(secondVods[0]) then
+		addVodLink(nil, match.vod, 1)
+		Array.forEach(secondVods[0], function(vodlink, vodindex)
+				addVodLink(nil, vodlink, vodindex + 1)
+			end)
+	else
+		addVodLink(nil, match.vod, nil)
+	end
+
+	-- Game Vods
+	for index, vod in pairs(vods) do
+		if Table.isNotEmpty(secondVods[index]) then
+			addVodLink(index, vod, 1)
+			Array.forEach(secondVods[index], function(vodlink, vodindex)
+				addVodLink(index, vodlink, vodindex + 1)
+			end)
+		else
+			addVodLink(index, vod, nil)
+		end
+	end
+
+	if Table.isNotEmpty(match.links) then
+		if Logic.isNotEmpty(vods) or match.vod then
+			footer:addElement(separator)
+		end
+	else
+		return footer
+	end
+
+	--- Platforms is used to keep the order of the links in footer
+	local platforms = mw.loadData('Module:MatchExternalLinks')
+	local links = match.links
+
+	local insertDotNext = false
+	local iconsInserted = 0
+
+	for _, platform in ipairs(platforms) do
+		if Logic.isNotEmpty(platform) then
+			local link = links[platform.name]
+			if link then
+				if insertDotNext then
+					insertDotNext = false
+					iconsInserted = 0
+					footer:addElement(separator)
+				end
+
+				local icon = platform.icon
+				local iconDark = platform.iconDark
+				local label = platform.label
+				local addGameLabel = platform.isMapStats and match.bestof and match.bestof > 1
+
+				for _, val in ipairs(link) do
+					addFooterLink(icon, iconDark, val[1], label, addGameLabel and val[2] or 0)
+					iconsInserted = iconsInserted + 1
+				end
+
+				if platform.stats then
+					for _, site in ipairs(platform.stats) do
+						if links[site] then
+							footer:addElement(separator)
+							break
+						end
+					end
+				end
+			end
+		else
+			insertDotNext = iconsInserted > 0 and true or false
+		end
+	end
+
+	return footer
 end
 
 ---@param game MatchGroupUtilGame
